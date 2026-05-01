9bf8edf2af42c20d41e0bdfac317b6d1e7205164
Almaz <almazamangaliev@gmail.com>
2026-05-01 14:45:56 +0500

    Fix custom domain Pages base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === "true" ? "/homerank/" : "/",
+  base: "./",
   plugins: [react()],
   server: {
     host: "127.0.0.1",
